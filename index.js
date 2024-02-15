8367a863739cdbc283d1d0e68b2c8883077f426d
Artemijs Matusev <matu6ev@gmail.com>
2024-02-15 15:24:32 +0000

    Set up EJS Template boilerplate

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//Root end point
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+//Start the server on port variable
 app.listen(`${port}`, () => {
     console.log("Server running on port 3000.");
-})
+});
