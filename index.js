5cad815a1de698999e3e602e8f67dda3786fea18
Artemijs Matusev <matu6ev@gmail.com>
2024-02-18 16:50:29 +0000

    Set up /retrieveVideo POST endpoint, and bodyparser to get hold of the request video URL

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 //Imports
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 //Middleware
 app.use(express.static("public")); //Set up the public static files folder
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//Root end point
+//Root endpoint
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
+
+
+//POSTING video link from front-end
+app.post("/retrieveVideo", (req, res) => {
+    const videoURL = req.body.videoURL; //Get hold of the video link from the request
+    console.log(videoURL);
+})
+
 
 //Start the server on port variable
 app.listen(`${port}`, () => {
